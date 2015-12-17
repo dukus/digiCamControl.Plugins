dea2002d0d8caa70bd3bcfd1b1462b9dfbf7ac41
Duka Istvan <dukusi@gmail.com>
2015-12-17 21:06:49 +0200

    Live view

--- Macrophotography/Macrophotography/LiveViewViewModel.cs
+++ Macrophotography/Macrophotography/LiveViewViewModel.cs
@@ -1,12 +1,229 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using CameraControl.Core;
+using CameraControl.Core.Classes;
+using CameraControl.Core.Translation;
+using CameraControl.Devices;
+using CameraControl.Devices.Classes;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Timer = System.Timers.Timer;
 
 namespace Macrophotography
 {
     public class LiveViewViewModel : ViewModelBase
     {
+        private bool _operInProgress = false;
+        private Timer _timer = new Timer(1000 / 25.0);
+
+        private ICameraDevice _cameraDevice;
+        private BitmapSource _bitmap;
+
+        public ICameraDevice CameraDevice
+        {
+            get { return _cameraDevice; }
+            set
+            {
+                _cameraDevice = value;
+                RaisePropertyChanged(() => CameraDevice);
+            }
+        }
+
+        public BitmapSource Bitmap
+        {
+            get { return _bitmap; }
+            set
+            {
+                _bitmap = value;
+                RaisePropertyChanged(() => Bitmap);
+            }
+        }
+        
+        public bool IsActive { get; set; }
+        public LiveViewData LiveViewData { get; set; }
+
+        public RelayCommand StartLiveViewCommand { get; set; }
+        public RelayCommand StopLiveViewCommand { get; set; }
+
+        public LiveViewViewModel()
+        {
+            StartLiveViewCommand = new RelayCommand(StartLiveView);
+            StopLiveViewCommand = new RelayCommand(StopLiveView);
+            _timer.AutoReset = true;
+            _timer.Elapsed += _timer_Elapsed;
+            CameraDevice = ServiceProvider.DeviceManager.SelectedCameraDevice;
+        }
+
+        void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Task.Factory.StartNew(GetLiveImage);
+        }
+
+        private void GetLiveImage()
+        {
+            if (_operInProgress)
+                return;
+
+            try
+            {
+                LiveViewData = CameraDevice.GetLiveViewImage();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error geting lv", ex);
+                _operInProgress = false;
+                return;
+            }
+
+            if (LiveViewData == null)
+            {
+                _operInProgress = false;
+                return;
+            }
+
+            try
+            {
+                if (LiveViewData != null && LiveViewData.ImageData != null)
+                {
+                    MemoryStream stream = new MemoryStream(LiveViewData.ImageData,
+                        LiveViewData.
+                            ImageDataPosition,
+                        LiveViewData.ImageData.
+                            Length -
+                        LiveViewData.
+                            ImageDataPosition);
+                    BitmapImage bi = new BitmapImage();
+                    bi.BeginInit();
+                    bi.CacheOption = BitmapCacheOption.OnLoad;
+                    bi.StreamSource = stream;
+                    bi.EndInit();
+                    bi.Freeze();
+                    Bitmap = bi;
+                    _operInProgress = false;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error geting lv", ex);
+                _operInProgress = false;
+                return;
+            }
+        }
+
+        private void StartLiveView()
+        {
+            //if (!IsActive)
+            //    return;
+
+            string resp = CameraDevice.GetProhibitionCondition(OperationEnum.LiveView);
+            if (string.IsNullOrEmpty(resp))
+            {
+                Thread thread = new Thread(StartLiveViewThread);
+                thread.Start();
+                thread.Join();
+            }
+            else
+            {
+                Log.Error("Error starting live view " + resp);
+                // in nikon case no show error message
+                // if the images not transferd yet from SDRam
+                if (resp != "LabelImageInRAM" && resp != "LabelCommandProcesingError")
+                    ServiceProvider.WindowsManager.ExecuteCommand(WindowsCmdConsts.LiveViewWnd_Message,
+                        TranslationStrings.LabelLiveViewError + "\n" +
+                        TranslationManager.GetTranslation(resp));
+            }
+        }
+
+        private void StartLiveViewThread()
+        {
+            try
+            {
+                bool retry = false;
+                int retryNum = 0;
+                Log.Debug("LiveView: Liveview started");
+                do
+                {
+                    try
+                    {
+                        CameraDevice.StartLiveView();
+                    }
+                    catch (DeviceException deviceException)
+                    {
+                        if (deviceException.ErrorCode == ErrorCodes.ERROR_BUSY ||
+                            deviceException.ErrorCode == ErrorCodes.MTP_Device_Busy)
+                        {
+                            Thread.Sleep(100);
+                            Log.Debug("Retry live view :" + deviceException.ErrorCode.ToString("X"));
+                            retry = true;
+                            retryNum++;
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                } while (retry && retryNum < 35);
+                _timer.Start();
+                _operInProgress = false;
+                Log.Debug("LiveView: Liveview start done");
+            }
+            catch (Exception exception)
+            {
+                Log.Error("Unable to start liveview !", exception);
+                StaticHelper.Instance.SystemMessage = "Unable to start liveview ! " + exception.Message;
+            }
+        }
+
+        private void StopLiveView()
+        {
+            if (!CameraDevice.IsChecked)
+                return;
+            Thread thread = new Thread(StopLiveViewThread);
+            thread.Start();
+        }
+
+        private void StopLiveViewThread()
+        {
+            try
+            {
+                bool retry = false;
+                int retryNum = 0;
+                Log.Debug("LiveView: Liveview stopping");
+                do
+                {
+                    try
+                    {
+                        CameraDevice.StopLiveView();
+                    }
+                    catch (DeviceException deviceException)
+                    {
+                        if (deviceException.ErrorCode == ErrorCodes.ERROR_BUSY ||
+                            deviceException.ErrorCode == ErrorCodes.MTP_Device_Busy)
+                        {
+                            Thread.Sleep(500);
+                            Log.Debug("Retry live view stop:" + deviceException.ErrorCode.ToString("X"));
+                            retry = true;
+                            retryNum++;
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                } while (retry && retryNum < 35);
+            }
+            catch (Exception exception)
+            {
+                Log.Error("Unable to stop liveview !", exception);
+                StaticHelper.Instance.SystemMessage = "Unable to stop liveview ! " + exception.Message;
+            }
+        }
     }
 }
